5e9d52cb05374c03ddf0ac6fc5a934135b17ae04
Andrija Feher <andrija.feher@gmail.com>
2014-08-04 03:22:22 +0200

    Add JSON database and read descriptions, and field of application for every node configuration. Change style

---
+++ js/data.js
@@ -0,0 +1,45 @@
+var somanetnodes = TAFFY([
+
+	// COM EtherCAT + CORE C22 + X
+	{"id":"000", "name":"100W EtherCAT Motor Controller", "foa":["industrial-robotics", "service-robotics", "industrial-automation", "medical"], "description":"Real-time multi axis control with EtherCAT is a no brainier"},
+	{"id":"001", "name":"300W EtherCAT Motor Controller", "foa":["industrial-robotics", "service-robotics", "industrial-automation", "medical"], "description":"Real-time multi axis control with EtherCAT is a no brainier"},
+	{"id":"002", "name":"EtherCAT Analog I/O Node", "foa":["industrial-robotics", "service-robotics", "industrial-automation", "medical"], "description":"Real-time Sensor data acquisition and actuation made easy with EtherCAT and the versatile GPIO-A module"},
+	{"id":"003", "name":"EtherCAT Digital I/O", "foa":["industrial-robotics", "service-robotics", "industrial-automation", "medical"], "description":""},
+	{"id":"004", "name":"EtherCAT to DX Gateway", "foa":[], "description":""},
+	{"id":"005", "name":"EtherCAT to CAN/RS-485/LIN Gateway", "foa":[], "description":""},
+
+	// CORE C21 + X
+	{"id":"N10", "name":"DX 100W Motor Controller Extension", "foa":[], "description":""},
+	{"id":"N11", "name":"DX 300W Motor Controller Extension", "foa":[], "description":""},
+	{"id":"N12", "name":"DX Analog I/O Extension", "foa":[], "description":""},
+	{"id":"N13", "name":"DX Digital I/O Extension", "foa":[], "description":""},
+	{"id":"N14", "name":"Pure waste of money", "foa":[], "description":""},
+	{"id":"N15", "name":"DX to CAN/RS-485/LIN Gateway", "foa":[], "description":""},
+
+	// COM Serial + CORE C22 + X
+	{"id":"100", "name":"100W CAN/RS-485/LIN Motor Controller", "foa":["industrial-automation","e-mobility"], "description":""},
+	{"id":"101", "name":"300W CAN/RS-485/LIN Motor Controller", "foa":["industrial-automation","e-mobility"], "description":""},
+	{"id":"102", "name":"CAN/RS-485/LIN Analog I/O Node", "foa":["industrial-automation"], "description":""},
+	{"id":"103", "name":"CAN/RS-485/LIN Digital I/O", "foa":["industrial-automation"], "description":""},
+	{"id":"104", "name":"CAN/RS-485/LIN Gateway to DX Gateway", "foa":["industrial-automation"], "description":""},
+	{"id":"105", "name":"CAN/RS-485/LIN Gateway to CAN/RS-485/LIN Gateway", "foa":["industrial-automation"], "description":""},
+
+	// COM WiFi + CORE C22 + X
+	{"id":"200", "name":"100W WiFi Motor Controller", "foa":["e-mobility"], "description":""},
+	{"id":"201", "name":"300W WiFi Motor Controller", "foa":["e-mobility"], "description":""},
+	{"id":"202", "name":"WiFi Analog I/O Node", "foa":[], "description":""},
+	{"id":"203", "name":"WiFi Digital I/O", "foa":[], "description":""},
+	{"id":"204", "name":"WiFi to DX Gateway", "foa":[], "description":""},
+	{"id":"205", "name":"WiFi to CAN/RS-485/LIN Gateway", "foa":[], "description":""},
+
+	// COM DX + CORE C22 + X
+	{"id":"300", "name":"DX 100W Motor Controller Extension", "foa":["industrial-robotics", "service-robotics"], "description":""},
+	{"id":"301", "name":"DX 300W Motor Controller Extension", "foa":["industrial-robotics", "service-robotics"], "description":""},
+	{"id":"302", "name":"DX Analog I/O Extension", "foa":[], "description":""},
+	{"id":"303", "name":"DX Digital I/O Extension", "foa":[], "description":""},
+	{"id":"304", "name":"Pure waste of money", "foa":[], "description":""},
+	{"id":"305", "name":"DX to CAN/RS-485/LIN Gateway", "foa":[], "description":""},
+]);
+
+
+
